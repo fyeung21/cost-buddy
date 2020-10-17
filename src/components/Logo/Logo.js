6b7c9da4ff777babf144ae3f9f0ac3dc8052f1f3
Fiona <fyeung21@gmail.com>
2020-10-16 17:24:54 -0700

    style: Style logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,10 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import { theme } from "../../globalStyles";
+
+export const LogoCont = styled.div`
+    margin: 1rem 0.5rem;
+`
+export const Title = styled.h1`
+    color: ${theme.headingFontColor};
+    font-family: ${theme.headingFont};
+    font-size: ${theme.h1FontSize};
+    font-weight: 800;
+`
 
 const Logo = () => {
     return (
-        <>
-            <h1>cost buddy</h1>
-        </>
+        <LogoCont>
+            <Title>cost buddy</Title>
+        </LogoCont>
     )
 }
 
