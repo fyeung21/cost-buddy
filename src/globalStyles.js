3fdf281f70858ca201536d5b6f48e9da1f946117
Fiona <fyeung21@gmail.com>
2020-10-16 17:21:54 -0700

    feat: Set up theme in global styles

---
+++ src/globalStyles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const theme = {
+  headingFont: `"Comfortaa", cursive`,
+  bodyFont: `"Roboto", sans-serif`,
+  bodyFontColor: "#FFF",
+  headingFontColor: "#FFF",
+  primaryColor: "#FFF",
+  buttonColor: "#50BDB4",
+  h1FontSize: "3.5rem",
+  h2FontSize: "2rem",
+  skinnyWindowSize: "414px",
+  tabletSize: "768px",
+  smallLaptopSize: "1024px",
+  desktopSize: "1200px",
+}
