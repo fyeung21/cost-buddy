f4a1075a6055c1212b5130dd3eca92ce23eca585
Fiona <fyeung21@gmail.com>
2020-10-17 23:47:39 -0700

    style: Basic styling for ItemCard

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,23 +1,39 @@
 import React from "react";
+import styled from "styled-components";
+import { theme } from "../../globalStyles";
+
+export const Display = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: #fff;
+    border-radius: 20px;
+    width: 1000px;
+    margin: 0 auto;
+    padding: 2rem;
+`
+export const Column = styled.div`
+    width: 300px;
+`
 
 const ItemCard = () => {
     return (
-        <>
-            <div>
+        <Display>
+            <Column>
                 <h1>{"description inline"}</h1>
                 <p>${" price"}</p>
-            </div>
+            </Column>
 
-            <div>
+            <Column>
                 <p>name with checkbox</p>
                 <p>name with checkbox</p>
                 <p>name with checkbox</p>
-            </div>
+            </Column>
 
-            <div>
+            <Column>
                 <p>{"split amount based on total and number of checkboxes checked. Need to round up."}</p>
-            </div>
-        </>
+            </Column>
+        </Display>
     )
 }
 
