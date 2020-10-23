ee0d6e730daab2c783b52a430c1f891cc6894406
Fiona <fyeung21@gmail.com>
2020-10-22 21:24:35 -0700

    feat: Set up item list for cards

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./reset.css";
 import './App.css';
 import Logo from './components/Logo/Logo';
-import ItemCard from './components/ItemCard/ItemCard';
+import ItemList from './components/ItemList/ItemList';
 
 const App = () => {
+  const [items, setItems] = useState([]);
+
+  const addItemHandler = (e) => {
+    e.preventDefault();
+        setItems([
+            ...items,
+            { id: Math.random() * 1000 }
+        ]);
+  }
+
   return (
     <div>
       <Logo/>
       <div>
-        <ItemCard />
+        <ItemList
+          items={items}
+          setItems={setItems}
+        />
       </div>
+        <button onClick={addItemHandler}>click here to add</button>
     </div>
   );
 }
---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ItemCard from "../ItemCard/ItemCard";
+
+const ItemList = ({items, setItems}) => {
+    return (
+        <div>
+            <ul>
+                {items.map((item) => (
+                    <ItemCard
+                        key={item.id}
+                        items={items}
+                        setItems={setItems}
+                    />
+                ))}
+            </ul>
+        </div>
+    )
+};
+
+export default ItemList;
