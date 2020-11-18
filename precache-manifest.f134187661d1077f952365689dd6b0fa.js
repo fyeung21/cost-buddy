2a264f7206d313dd514a32c1af5141e9d5fb4429
Fiona <fyeung21@gmail.com>
2020-11-17 20:18:22 -0800

    Updates

---
+++ precache-manifest.f134187661d1077f952365689dd6b0fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5644ead3dd2082f164994e34133100fb",
+    "url": "/cost-buddy/index.html"
+  },
+  {
+    "revision": "210ceed8eb9cfa5ae4d0",
+    "url": "/cost-buddy/static/css/main.8f86c898.chunk.css"
+  },
+  {
+    "revision": "df77c7167b25cd6fb7f7",
+    "url": "/cost-buddy/static/js/2.48808913.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cost-buddy/static/js/2.48808913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "210ceed8eb9cfa5ae4d0",
+    "url": "/cost-buddy/static/js/main.fc852389.chunk.js"
+  },
+  {
+    "revision": "04b1df94a57e7fab4d5d",
+    "url": "/cost-buddy/static/js/runtime-main.a68f021f.js"
+  }
+]);
